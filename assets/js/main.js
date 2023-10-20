a60d85a2ea1c4a67adce6bf5bcbe506b3a2e4458
Johnnyadee <johnadeoluwai@yahoo.com>
2023-10-20 22:57:43 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -73,6 +73,49 @@
     onscroll(document, toggleBacktotop);
   }
 
+class Header extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = ` <header id="header" class="fixed-top d-flex align-items-center">
+      <div class="container d-flex align-items-center">
+        <a href="index.html" class="logo "><img src="assets/img/logo.png" alt="" class="img-fluid"></a>
+        <h1 class="logo me-auto"><a href="index.html">Saint Flairs Awards</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- -->
+
+        <nav id="navbar" class="navbar">
+          <ul>
+            <li><a href="index.html" id="home">Home</a></li>
+
+            <li class="dropdown">
+              <a href="#" id="about"><span>About</span> <i class="bi bi-chevron-down"></i></a>
+              <ul>
+                <li><a href="about.html">About</a></li>
+                <li><a href="about.html#team">Team</a></li>
+
+                <!-- <li class="dropdown">
+                  <a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                  <ul>
+                    <li><a href="#">Deep Drop Down 1</a></li>
+                    <li><a href="#">Deep Drop Down 2</a></li>
+                    <li><a href="#">Deep Drop Down 3</a></li>
+                    <li><a href="#">Deep Drop Down 4</a></li>
+                    <li><a href="#">Deep Drop Down 5</a></li>
+                  </ul>
+                </li> -->
+              </ul>
+            </li>
+            <li><a href="services.html" id="products">Our Products</a></li>
+            <li><a href="#" id="moments">Golden Moments</a></li>
+            <li><a href="#" class="getstarted">Contact us</a></li>
+          </ul>
+          <i class="bi bi-list mobile-nav-toggle"></i>
+        </nav>
+        <!-- .navbar -->
+      </div>
+    </header>`;
+  }
+}
+customElements.define("main-header", Header);
   /**
    * Mobile nav toggle
    */
@@ -195,49 +238,6 @@
   }
 })();
 
-class Header extends HTMLElement {
-  connectedCallback() {
-    this.innerHTML = ` <header id="header" class="fixed-top d-flex align-items-center">
-      <div class="container d-flex align-items-center">
-        <a href="index.html" class="logo "><img src="assets/img/logo.png" alt="" class="img-fluid"></a>
-        <h1 class="logo me-auto"><a href="index.html">Saint Flairs Awards</a></h1>
-        <!-- Uncomment below if you prefer to use an image logo -->
-        <!-- -->
-
-        <nav id="navbar" class="navbar">
-          <ul>
-            <li><a href="index.html" id="home">Home</a></li>
-
-            <li class="dropdown">
-              <a href="#" id="about"><span>About</span> <i class="bi bi-chevron-down"></i></a>
-              <ul>
-                <li><a href="about.html">About</a></li>
-                <li><a href="about.html#team">Team</a></li>
-
-                <!-- <li class="dropdown">
-                  <a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
-                  <ul>
-                    <li><a href="#">Deep Drop Down 1</a></li>
-                    <li><a href="#">Deep Drop Down 2</a></li>
-                    <li><a href="#">Deep Drop Down 3</a></li>
-                    <li><a href="#">Deep Drop Down 4</a></li>
-                    <li><a href="#">Deep Drop Down 5</a></li>
-                  </ul>
-                </li> -->
-              </ul>
-            </li>
-            <li><a href="services.html" id="products">Our Products</a></li>
-            <li><a href="portfolio.html" id="moments">Golden Moments</a></li>
-            <li><a href="contact.html" class="getstarted">Contact us</a></li>
-          </ul>
-          <i class="bi bi-list mobile-nav-toggle"></i>
-        </nav>
-        <!-- .navbar -->
-      </div>
-    </header>`;
-  }
-}
-customElements.define("main-header", Header);
 
 class Footer extends HTMLElement {
   connectedCallback() {
